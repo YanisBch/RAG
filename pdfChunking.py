fb71490c7b8bc63faccfc3a868a65d99be3816b1
Yanis Bch <yanisbouch49@gmail.com>
2025-11-26 21:48:08 +0100

    Documentation update

--- pdfChunking.py
+++ pdfChunking.py
@@ -1,7 +1,18 @@
 from PyPDF2 import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+
 def pdfChunking(filePath):
+    """
+    Split a pdf into chunk of similar size 
+
+    Args:
+        filePath (string): Path of the document to insert into the db
+
+    Returns:
+        texts: List of str representing all the chunks of the pdf
+
+    """
     document = PdfReader(open(filePath, 'rb'))
     pdftext = ""
     
