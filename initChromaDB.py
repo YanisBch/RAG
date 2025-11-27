a835c2e4c858d7d951f6ce81ee14d610fb89dbc6
Yanis Bch <yanisbouch49@gmail.com>
2025-11-27 18:21:44 +0100

    DB Initialisation

---
+++ initChromaDB.py
@@ -0,0 +1,12 @@
+import chromadb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+dbDirectory = os.getenv("DB_DIRECTORY")
+client = chromadb.PersistentClient(dbDirectory)
+
+client = chromadb.PersistentClient(dbDirectory)
+
+collection = client.create_collection("documents")
+
