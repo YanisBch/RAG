29e4311da111212ca4b437cbe6dfc7674b61bc59
Yanis Bch <yanisbouch49@gmail.com>
2025-11-27 08:35:25 +0100

    RAG processus successful

--- rag.py
+++ rag.py
@@ -1,14 +1,19 @@
 from geminiApiManager import completionRequest
 from chromaManager import searchQuery, addDocumentDB
+from pdfChunking import pdfChunking
 
 def promptGeneration(retrieve):
     similarDocuments = searchQuery(retrieve)
     similarDocumentsText = ""
     
-    for query in searchQuery:
-        similarDocumentsText = query['ids'] + query['document'] + " "
-    
+    for i in range(len(similarDocuments['ids'][0])):
+        similarDocumentsText = similarDocuments['ids'][0][i] + similarDocuments['documents'][0][i] + " "
+        
     prompt = "Answer the question: " + retrieve + " Base your answer on these documents: " + similarDocumentsText
     
     return prompt
+
+addDocumentDB('lessonsPDF/jules_verne_20000_lieues_sous_les_mers.pdf')
+
+print(completionRequest(promptGeneration("Pourrais tu me donner la description de Monsieur Pierre Aronnax")))
 
